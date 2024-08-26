ecea24bd45fb6f0cee33a88a39cf214a00b9bc81
TeamInfinity2 <Fabihanaseem2@gmail.com>
2024-08-26 23:26:20 +0500

    Add files via upload
    Signed-off-by: TeamInfinity2 <Fabihanaseem2@gmail.com>

---
+++ assigment js/app.js
@@ -0,0 +1,35 @@
+// Chapter 4
+// Q1
+// var var1 = "Hello", var2 = 10, var3 = true;
+// Q2
+// var myVariable;
+// var $price;
+// var _totalAmount;
+// var user1;
+// var first_name;
+// Q3
+// var 1stVariable; 
+// var my-variable; 
+// var alert; 
+// var var; 
+// var @user; 
+// q3 b
+// A variable name can contain only letters, numbers, dollar signs, and underscores
+// Q3C
+// c) Variables must begin with a letter, 
+// d) Variable names are case sensitive.
+// e) Variable names should not be JS keywords.
+// chapter 5
+// Q3
+// var num1 = 5;
+// var num2 = 10;
+// var sum = num1+num2;
+// document.write("The sum of " + num1 + " and " + num2 + " is: " + sum);
+
+// var num = 10;
+// alert(num++)
+
+// var num = 2 + 2 * 4 - 1;
+// alert(num)
+
+
