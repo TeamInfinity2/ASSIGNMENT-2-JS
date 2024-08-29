dd9d8829756afa032d846be640218e520a01c4ab
TeamInfinity2 <Fabihanaseem2@gmail.com>
2024-08-30 01:19:16 +0500

    Add files via upload
    Signed-off-by: TeamInfinity2 <Fabihanaseem2@gmail.com>

---
+++ assign 3/app.js
@@ -0,0 +1,86 @@
+// alert("hello world")
+// var num1 = "5";
+// var num2 = 7;
+// var num1 = +prompt("enter the 1st digit");
+// var num2 = +prompt("enter the 2nd digit")
+// console.log(num1 * num2)
+
+// var totalNum = num1  * num2;
+// alert(totalNum)
+// var heading = "<h1>hello world </h1>";
+// document.write(heading)
+// var num = 1;
+// alert("You have visited my site " + num + " times")
+// var num = 5;
+// alert(num)
+// num--;
+// alert(num)
+// num--;
+// alert(num)
+// ===assignment3====
+// Q1
+// var a = 10
+// alert("the value of a is 10")
+// ++a;
+// alert("the value of ++a is 11")
+// a++;
+// alert("the value of a++ is 12")
+// --a;
+// alert("the value of --a is 11")
+// a--;
+// alert("the value of a is 10")
+// q2
+// var a=2,b=3;
+// var result = --a - --b + ++b + b--;
+// --a;
+// alert("1")
+// --a - --b;
+// alert("-1")
+// --a - --b + ++b;
+// alert("3")
+// --a - --b + ++b + b--;
+// alert("1")
+// Q3
+// function greet(name) {
+//     return "Hello, " + name;
+// }
+// console.log(greet("Alice")); 
+// Q4
+// let name = +prompt("Enter your name:");
+// document.write(`<h1>Hello, ${name}!</h1>`);
+// Q5
+// let num = +prompt("Enter a number:");
+// if (num === null || num === "") {
+//   num = 5;
+// }
+// document.write(`<h1>Multiplication Table of ${num}</h1>`);
+// for (let i = 1; i <= 10; i++) {
+//   document.write(`${num} x ${i} = ${num * i}<br>`);
+// }
+// Q6
+// A
+// let subject1 = +prompt("Enter subject 2 name:");
+// let subject2 = +prompt("Enter subject 2 name:");
+// let subject3 = +prompt("Enter subject 3 name:");
+// B
+// let obtainedMarks1 = +prompt(`Enter obtained marks for ${subject1}:`);
+
+// document.write(`<h1>Subjects and Marks</h1>`);
+// document.write(`<p>${subject1}: ${obtainedMarks1}/${100}</p>`);
+// document.write(`<p>${subject2}: ?/${100}</p>`);
+// document.write(`<p>${subject3}: ?/${100}</p>`);
+// // D
+let obtainedMarks2 = +prompt(`Enter obtained marks for ${subject2}:`);
+let obtainedMarks3 = +prompt(`Enter obtained marks for ${subject3}:`);
+// E
+// let totalObtainedMarks = parseInt(obtainedMarks1) + parseInt(obtainedMarks2) + parseInt(obtainedMarks3);
+// let totalMarks = 100 * 3; // Total marks for 3 subjects
+// let percentage = (totalObtainedMarks / 100) * 100;
+
+// document.write(`<h1>Result</h1>`);
+// document.write(`<p>Total Marks: ${100}</p>`);
+// document.write(`<p>Total Obtained Marks: ${totalObtainedMarks}</p>`);
+// document.write(`<p>Percentage: ${percentage.toFixed(2)}%</p>`);
+
+
+
